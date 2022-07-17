2b67c212edefc3de0b71dff8d133b6dda1fa4ffe
Dima_Pechenenko <pechenekod@gmail.com>
2022-07-17 18:05:01 +0300

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Dmitro Pechenenko on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
